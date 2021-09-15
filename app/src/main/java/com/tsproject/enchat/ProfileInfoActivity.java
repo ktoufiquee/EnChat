0933931a62d3b15d41edf272f3f20ae5d88923e8
Tasnuva29 <78658046+Tasnuva29@users.noreply.github.com>
2021-09-15 09:14:35 +0600

    Profile Info

--- app/src/main/java/com/tsproject/enchat/ProfileInfoActivity.java
+++ app/src/main/java/com/tsproject/enchat/ProfileInfoActivity.java
@@ -1,35 +1,64 @@
 package com.tsproject.enchat;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileInfoActivity extends AppCompatActivity {
-    Button btnNext;
+    Button btnFinish;
     EditText etName;
+    FirebaseAuth auth;
+    FirebaseStorage storage;
+    FirebaseDatabase db;
+    CircleImageView civProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_info);
-        btnNext = findViewById(R.id.btnProfileNext);
+        btnFinish = findViewById(R.id.btnProfileNext);
         etName = findViewById(R.id.etName);
-        btnNext.setOnClickListener(new View.OnClickListener() {
+        civProfile = findViewById(R.id.ivProfilePhoto);
+
+        db = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+        auth = FirebaseAuth.getInstance();
+        civProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ImagePicker.with(ProfileInfoActivity.this)
+                        .crop()
+                        .compress(1024)
+                        .maxResultSize(1080, 1080)
+                        .start();
+                Log.d("Camera", "onClick: permission given");
+                Log.d("Camera", "onClick: permisison denied");
+            }
+        });
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
                 String rec_name = etName.getText().toString().trim();
                 if (rec_name.equals("")) {
                     etName.setError("Enter your name");
@@ -59,5 +88,14 @@ public class ProfileInfoActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri uri = data.getData();
+        Log.d("CAMERA", "onActivityResult: "+uri);
+        civProfile.setImageURI(uri);
+
     }
 }
